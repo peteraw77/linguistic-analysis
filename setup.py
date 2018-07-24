5bbb43d285e5ce065575808d42084de437b7f393
Pete Wright <pewright77@gmail.com>
2018-07-24 13:52:08 -0400

    Increment alpha number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='linguistic_analysis',
-      version='1.0-alpha1',
+      version='1.0-alpha2',
       description='Tools for extracting meaning from text',
       author='Peter Wright',
       license='MIT',
